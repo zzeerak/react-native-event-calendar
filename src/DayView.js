32ba06ab43a75122c719900720a14b42f8b0fcaf
zzeerak <71652055+zzeerak@users.noreply.github.com>
2021-03-18 17:03:48 +0500

    Update DayView.js

--- src/DayView.js
+++ src/DayView.js
@@ -1,217 +1,236 @@
 // @flow
-import { View, Text, ScrollView, TouchableOpacity,TouchableWithoutFeedback } from 'react-native';
-import populateEvents from './Packer';
-import React from 'react';
-import moment from 'moment';
-import _ from 'lodash';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import populateEvents from './Packer'
+import React from 'react'
+import moment from 'moment'
+import _ from 'lodash'
+import { logData } from '@/Helpers/DebugHelper'
 
-const LEFT_MARGIN = 60 - 1;
+const LEFT_MARGIN = 60 - 1
 // const RIGHT_MARGIN = 10
-const CALENDER_HEIGHT = 2400;
+const CALENDER_HEIGHT = 2400
 // const EVENT_TITLE_HEIGHT = 15
-const TEXT_LINE_HEIGHT = 17;
+const TEXT_LINE_HEIGHT = 17
 // const MIN_EVENT_TITLE_WIDTH = 20
 // const EVENT_PADDING_LEFT = 4
 
 function range(from, to) {
-  return Array.from(Array(to), (_, i) => from + i);
+    let arr = Array.from(Array(to), (_, i) => from + i)
+    return arr
 }
 
 export default class DayView extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.calendarHeight = (props.end - props.start) * 100;
-    const width = props.width - LEFT_MARGIN;
-    const packedEvents = populateEvents(props.events, width, props.start);
-    let initPosition =
-      _.min(_.map(packedEvents, 'top')) -
-      this.calendarHeight / (props.end - props.start);
-    initPosition = initPosition < 0 ? 0 : initPosition;
-    this.state = {
-      _scrollY: initPosition,
-      packedEvents,
-    };
-  }
+    constructor(props) {
+        super(props)
+        this.calendarHeight = (props.end - props.start) * 100
+        const width = props.width - LEFT_MARGIN
+        const packedEvents = populateEvents(props.events, width, props.start)
+        let initPosition =
+            _.min(_.map(packedEvents, 'top')) -
+            this.calendarHeight / (props.end - props.start)
+        initPosition = initPosition < 0 ? 0 : initPosition
+        this.state = {
+            _scrollY: initPosition,
+            packedEvents,
+        }
+    }
 
-  componentWillReceiveProps(nextProps) {
-    const width = nextProps.width - LEFT_MARGIN;
-    this.setState({
-      packedEvents: populateEvents(nextProps.events, width, nextProps.start),
-    });
-  }
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        const width = nextProps.width - LEFT_MARGIN
+        this.setState({
+            packedEvents: populateEvents(nextProps.events, width, nextProps.start),
+        })
+    }
 
-  componentDidMount() {
-    this.props.scrollToFirst && this.scrollToFirst();
-  }
+    componentDidMount() {
+        this.props.scrollToFirst && this.scrollToFirst()
+    }
 
-  scrollToFirst() {
-    setTimeout(() => {
-      if (this.state && this.state._scrollY && this._scrollView) {
-        this._scrollView.scrollTo({
-          x: 0,
-          y: this.state._scrollY,
-          animated: true,
-        });
-      }
-    }, 1);
-  }
+    scrollToFirst() {
+        setTimeout(() => {
+            if (this.state && this.state._scrollY && this._scrollView) {
+                this._scrollView.scrollTo({
+                    x: 0,
+                    y: this.state._scrollY,
+                    animated: true,
+                })
+            }
+        }, 1)
+    }
 
-  _renderRedLine() {
-    const offset = 100;
-    const { format24h } = this.props;
-    const { width, styles } = this.props;
-    const timeNowHour = moment().hour();
-    const timeNowMin = moment().minutes();
-    return (
-      <View
-        key={`timeNow`}
-        style={[
-          styles.lineNow,
-          {
-            top:
-              offset * (timeNowHour - this.props.start) +
-              (offset * timeNowMin) / 60,
-            width: width - 20,
-          },
-        ]}
-      />
-    );
-  }
+    _renderRedLine() {
+        const offset = 100
+        const { format24h } = this.props
+        const { width, styles } = this.props
+        const timeNowHour = moment().hour()
+        const timeNowMin = moment().minutes()
+        return (
+            <View
+                key={`timeNow`}
+                style={[
+                    styles.lineNow,
+                    {
+                        top:
+                            offset * (timeNowHour - this.props.start) +
+                            (offset * timeNowMin) / 60,
+                        width: width - 20,
+                    },
+                ]}
+            />
+        )
+    }
 
-  _renderLines() {
-    const { format24h, start, end } = this.props;
-    const offset = this.calendarHeight / (end - start);
+    _renderLines() {
+        const { format24h, start, end } = this.props
+        const offset = this.calendarHeight / (end - start)
 
-    return range(start, end + 1).map((i, index) => {
-      let timeText;
-      if (i === start) {
-        timeText = ``;
-      } else if (i < 12) {
-        timeText = !format24h ? `${i} AM` : i;
-      } else if (i === 12) {
-        timeText = !format24h ? `${i} PM` : i;
-      } else if (i === 24) {
-        timeText = !format24h ? `12 AM` : 0;
-      } else {
-        timeText = !format24h ? `${i - 12} PM` : i;
-      }
-      const { width, styles } = this.props;
-      return [
-          <TouchableOpacity           key={`timeLabel${i}`}
-                                      onPress={()=>this.props.dateTouched(timeText,this.props.date)}>
-        <Text
-          style={[styles.timeLabel, { top: offset * index - 6 }]}
-        >
-          {timeText}
-        </Text>
-          </TouchableOpacity>
-            ,
-        i === start ? null : (
-          <View
-            key={`line${i}`}
-            style={[styles.line, { top: offset * index, width: width - 20 }]}
-          />
-        ),
-        <View
-          key={`lineHalf${i}`}
-          style={[
-            styles.line,
-            { top: offset * (index + 0.5), width: width - 20 },
-          ]}
-        />,
-      ];
-    });
-  }
+        return range(start, end + 1).map((i, index) => {
+            let timeText
+            let halfTime = ''
+            if (i === start) {
+                timeText = ``
+            } else if (i < 12) {
+                timeText = !format24h ? `${i} AM` : i
+            } else if (i === 12) {
+                timeText = !format24h ? `${i} PM` : i
+            } else if (i === 24) {
+                timeText = !format24h ? `12 AM` : 0
+            } else {
+                timeText = !format24h ? `${i - 12} PM` : i
+            }
+            halfTime = (timeText.split(' ')[0] === '' ? '12' : timeText.split(' ')[0]) + ':' + '30' + ' ' + (timeText.split(' ')[1] ? timeText.split(' ')[1] : 'AM')
+            const { width, styles } = this.props
+            return [
+                <TouchableOpacity
+                    key={`timeLabel${i}`}
+                    onPress={() => this.props.dateTouched(timeText, this.props.date)}>
+                    <Text
+                        style={[styles.timeLabel, { top: offset * index - 6 }]}
+                    >
+                        {timeText}
+                    </Text>
+                </TouchableOpacity>
+                ,
+                i === start ? null : (
+                    <View
+                        key={`line${i}`}
+                        style={[styles.line, { top: offset * index, width: width - 20,marginStart: 15 }]}
+                    />
+                ),
+                <TouchableOpacity
+                    key={`lineHalf${i}`}
+                    onPress={() => this.props.dateTouched(halfTime, this.props.date)}>
+                    <View style={[{ flexDirection: 'row' }]}>
+                        <Text
+                            style={[styles.timeLabel, { top: offset * (index + 0.45) }]}
+                        >
+                            {halfTime}
+                        </Text>
+                        <View
 
-  _renderTimeLabels() {
-    const { styles, start, end } = this.props;
-    const offset = this.calendarHeight / (end - start);
-    return range(start, end).map((item, i) => {
-      return (
-        <View key={`line${i}`} style={[styles.line, { top: offset * i }]} />
-      );
-    });
-  }
+                            style={[
+                                styles.line,
+                                { marginStart: 15, top: offset * (index + 0.5), width: width - 30 },
+                            ]}
+                        >
 
-  _onEventTapped(event) {
-    this.props.eventTapped(event);
-  }
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                ,
+            ]
+        })
+    }
 
-  _renderEvents() {
-    const { styles } = this.props;
-    const { packedEvents } = this.state;
-    let events = packedEvents.map((event, i) => {
-      const style = {
-        left: event.left,
-        height: event.height,
-        width: event.width,
-        top: event.top,
-      };
+    _renderTimeLabels() {
+        const { styles, start, end } = this.props
+        const offset = this.calendarHeight / (end - start)
+        return range(start, end).map((item, i) => {
+            return (
+                <View key={`line${i}`} style={[styles.line, { top: offset * i }]}/>
+            )
+        })
+    }
 
-      const eventColor = {
-        backgroundColor: event.color,
-      };
+    _onEventTapped(event) {
+        this.props.eventTapped(event)
+    }
 
-      // Fixing the number of lines for the event title makes this calculation easier.
-      // However it would make sense to overflow the title to a new line if needed
-      const numberOfLines = Math.floor(event.height / TEXT_LINE_HEIGHT);
-      const formatTime = this.props.format24h ? 'HH:mm' : 'hh:mm A';
-      return (
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() =>
-            this._onEventTapped(this.props.events[event.index])
-          }
-          key={i} style={[styles.event, style, event.color && eventColor]}
-        >
-          {this.props.renderEvent ? (
-            this.props.renderEvent(event)
-          ) : (
-            <View>
-              <Text numberOfLines={1} style={styles.eventTitle}>
-                {event.title || 'Event'}
-              </Text>
-              {numberOfLines > 1 ? (
-                <Text
-                  numberOfLines={numberOfLines - 1}
-                  style={[styles.eventSummary]}
+    _renderEvents() {
+        const { styles } = this.props
+        const { packedEvents } = this.state
+        let events = packedEvents.map((event, i) => {
+            const style = {
+                left: event.left,
+                height: event.height,
+                width: event.width,
+                top: event.top,
+            }
+
+            const eventColor = {
+                backgroundColor: event.color,
+            }
+
+            // Fixing the number of lines for the event title makes this calculation easier.
+            // However it would make sense to overflow the title to a new line if needed
+            const numberOfLines = Math.floor(event.height / TEXT_LINE_HEIGHT)
+            const formatTime = this.props.format24h ? 'HH:mm' : 'hh:mm A'
+            return (
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    onPress={() =>
+                        this._onEventTapped(this.props.events[event.index])
+                    }
+                    key={i} style={[styles.event, style, event.color && eventColor]}
                 >
-                  {event.summary || ' '}
-                </Text>
-              ) : null}
-              {numberOfLines > 2 ? (
-                <Text style={styles.eventTimes} numberOfLines={1}>
-                  {moment(event.start).format(formatTime)} -{' '}
-                  {moment(event.end).format(formatTime)}
-                </Text>
-              ) : null}
-              </View>
-          )}
-        </TouchableOpacity>
-      );
-    });
+                    {this.props.renderEvent ? (
+                        this.props.renderEvent(event)
+                    ) : (
+                        <View>
+                            <Text numberOfLines={1} style={styles.eventTitle}>
+                                {event.title || 'Event'}
+                            </Text>
+                            {numberOfLines > 1 ? (
+                                <Text
+                                    numberOfLines={numberOfLines - 1}
+                                    style={[styles.eventSummary]}
+                                >
+                                    {event.summary || ' '}
+                                </Text>
+                            ) : null}
+                            {numberOfLines > 2 ? (
+                                <Text style={styles.eventTimes} numberOfLines={1}>
+                                    {moment(event.start).format(formatTime)} -{' '}
+                                    {moment(event.end).format(formatTime)}
+                                </Text>
+                            ) : null}
+                        </View>
+                    )}
+                </TouchableOpacity>
+            )
+        })
 
-    return (
-      <View>
-        <View style={{ marginLeft: LEFT_MARGIN }}>{events}</View>
-      </View>
-    );
-  }
+        return (
+            <View>
+                <View style={{ marginLeft: LEFT_MARGIN }}>{events}</View>
+            </View>
+        )
+    }
 
-  render() {
-    const { styles } = this.props;
-    return (
-      <ScrollView
-        ref={ref => (this._scrollView = ref)}
-        contentContainerStyle={[
-          styles.contentStyle,
-          { width: this.props.width },
-        ]}
-      >
-        {this._renderLines()}
-        {this._renderEvents()}
-        {this._renderRedLine()}
-      </ScrollView>
-    );
-  }
+    render() {
+        const { styles } = this.props
+        return (
+            <ScrollView
+                ref={ref => (this._scrollView = ref)}
+                contentContainerStyle={[
+                    styles.contentStyle,
+                    { width: this.props.width },
+                ]}
+            >
+                {this._renderLines()}
+                {this._renderEvents()}
+                {this._renderRedLine()}
+            </ScrollView>
+        )
+    }
 }
